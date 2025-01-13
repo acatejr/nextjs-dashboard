ec316ddb398dd6c0f5b35401457a4cb3a99f1a88
acatejr <acatejr@gmail.com>
2025-01-13 08:29:32 -0700

    Fixed create invoice bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,4 @@
-'use server'
+'use server';
 
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
@@ -19,11 +19,11 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
-    const { customerId, amount, status } = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
+    });
 
     const amountInCents = amount * 100
     const date = new Date().toISOString().split('T')[0]
@@ -52,9 +52,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-  }
+}
 
-  export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-  }
+}
